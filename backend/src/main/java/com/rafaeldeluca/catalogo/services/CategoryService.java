f051162397323e69121f7f1b63e702a0314048cc
Rafael de Luca <deluca1712@gmail.com>
2022-05-24 15:58:28 -0300

    database load initial data - seeding

--- backend/src/main/java/com/rafaeldeluca/catalogo/services/CategoryService.java
+++ backend/src/main/java/com/rafaeldeluca/catalogo/services/CategoryService.java
@@ -1,10 +1,10 @@
 package com.rafaeldeluca.catalogo.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.rafaeldeluca.catalogo.entities.Category;
 import com.rafaeldeluca.catalogo.repositories.CategoryRepository;
@@ -16,6 +16,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
 		return repository.findAll();
 	}
---
+++ backend/src/main/java/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO  tb_category(name) VALUES ('Notebooks');
+
+INSERT INTO  tb_category(name) VALUES ('Books');
+
+INSERT INTO  tb_category(name) VALUES ('Eletronics');
