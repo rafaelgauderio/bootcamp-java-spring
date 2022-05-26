0e85ebe60d0645e6a35779c1fa5a34ea177a5b9c
Rafael de Luca <deluca1712@gmail.com>
2022-05-26 11:36:38 -0300

    InsertCategory - POST new Category

--- backend/src/main/java/com/rafaeldeluca/catalogo/resources/CategoryResource.java
+++ backend/src/main/java/com/rafaeldeluca/catalogo/resources/CategoryResource.java
@@ -1,13 +1,17 @@
 package com.rafaeldeluca.catalogo.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rafaeldeluca.catalogo.dto.CategoryDTO;
 import com.rafaeldeluca.catalogo.services.CategoryService;
@@ -30,6 +34,16 @@ public class CategoryResource {
 	public ResponseEntity <CategoryDTO> findById(@PathVariable Long id) {		
 		CategoryDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	//codigo http 200 = success
+	//codigo http 201 = recurso criado
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insertCategory(@RequestBody CategoryDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 	
 }
--- backend/src/main/java/com/rafaeldeluca/catalogo/services/CategoryService.java
+++ backend/src/main/java/com/rafaeldeluca/catalogo/services/CategoryService.java
@@ -1,12 +1,17 @@
 package com.rafaeldeluca.catalogo.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rafaeldeluca.catalogo.dto.CategoryDTO;
 import com.rafaeldeluca.catalogo.entities.Category;
@@ -37,5 +42,12 @@ public class CategoryService {
 		Category entity = object.orElseThrow(() -> new EntityNotFoundException("Entidade não foi encontrada!"));
 		return new CategoryDTO(entity);
 	}
+
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+	}	
 
 }
