9283bec3209d9f65ec36e1597f46f37f380aee8e
Rafael de Luca <deluca1712@gmail.com>
2022-08-19 16:32:26 -0300

    Fixtures BeforEach, AfterEach

--- backend/src/test/java/com/rafaeldeluca/catalogo/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/rafaeldeluca/catalogo/repositories/ProductRepositoryTests.java
@@ -3,6 +3,7 @@ package com.rafaeldeluca.catalogo.repositories;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -16,26 +17,32 @@ public class ProductRepositoryTests {
 	@Autowired
 	private ProductRepository repository;
 	
+	private Long existId;
+	private Long nonExistId;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		this.existId = 2L;
+		this.nonExistId = 50L;
+	}
+	
 	@Test
-	public void deleteShouldDeleteObjectWhenIdExists() {		
-		
-		long idExist = 2L;
+	public void deleteShouldDeleteObjectWhenIdExists() {	
 		
 		repository.deleteById(2L);
 		
 		//optinal tem que retornar vazio se o produto de id correspondente foi deletado
-		Optional<Product> optional = repository.findById(idExist);
+		Optional<Product> optional = repository.findById(existId);
 		Assertions.assertFalse(optional.isPresent());
 		
 	}
 	
 	@Test
-	public void deleteShouldThrowExceptionWhenIdDoesNotExists () {
+	public void deleteShouldThrowExceptionWhenIdDoesNotExists () {	
 		
-		long nonExistingId = 50L;
 		
 		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
-			repository.deleteById(nonExistingId);
+			repository.deleteById(nonExistId);
 		});
 		
 	}	
