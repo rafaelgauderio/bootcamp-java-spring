c01d76cf6f8b0e66a5e258af34a9f2781b62ce43
Rafael de Luca <deluca1712@gmail.com>
2022-08-19 16:01:32 -0300

    Testing delete product by id

--- backend/src/test/java/com/rafaeldeluca/catalogo/CatalogoApplicationTests.java
+++ backend/src/test/java/com/rafaeldeluca/catalogo/CatalogoApplicationTests.java
@@ -8,6 +8,11 @@ class CatalogoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		/* contenxto em um aplicação Spring são todos os componentes, infraestrutura básica de um projeto Spring.
+		* fica lento carregar ele em todo o texto unitário
+		* Carrendo o contexto da aplicação é lento, é usado em TESTE DE INTEGRAÇÃO.
+		*/
 	}
+	
 
 }
---
+++ backend/src/test/java/com/rafaeldeluca/catalogo/repositories/ProductRepositoryTests.java
@@ -0,0 +1,31 @@
+package com.rafaeldeluca.catalogo.repositories;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.rafaeldeluca.catalogo.entities.Product;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	@Test
+	public void deleteShouldDeleteObjectWhenIdExists() {		
+		
+		long idExist = 2L;
+		
+		repository.deleteById(2L);
+		
+		//optinal tem que retornar vazio se o produto de id correspondente foi deletado
+		Optional<Product> optional = repository.findById(idExist);
+		Assertions.assertFalse(optional.isPresent());
+		
+	}
+
+}
