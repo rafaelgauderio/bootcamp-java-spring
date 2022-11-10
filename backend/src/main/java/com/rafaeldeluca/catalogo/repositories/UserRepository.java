416e3dd368ed56c780091928b3cddff28cdc6c02
Rafael de Luca <deluca1712@gmail.com>
2022-11-10 15:15:40 -0300

    Query method find User by first Name

--- backend/src/main/java/com/rafaeldeluca/catalogo/repositories/UserRepository.java
+++ backend/src/main/java/com/rafaeldeluca/catalogo/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.rafaeldeluca.catalogo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +12,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	
 	//Método para buscar no database um usuario por email	
 	User findByEmail(String email);	
+	
+	//busca por firstName query method
+	List<User> findByFirstNameIgnoreCase(String firstName);
 
 }
