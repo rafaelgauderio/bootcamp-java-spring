ae8fa017f4ba1129d1bc2899aab2e8aef6e173d4
Rafael De Luca <deluca1712@gmail.com>
2022-11-13 12:46:03 -0300

    filter produtos, query with categoryId, SELECT DISTINCT, NULL

--- backend/src/main/java/com/rafaeldeluca/catalogo/repositories/ProductRepository.java
+++ backend/src/main/java/com/rafaeldeluca/catalogo/repositories/ProductRepository.java
@@ -12,9 +12,15 @@ import com.rafaeldeluca.catalogo.entities.Product;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	
-	@Query("SELECT objeto FROM Product objeto "
+	// Se a category é null, então não foi informado o parâmetro, então tem que buscar todas produtos sem filtar por
+	// 	categoriaId, e não retornar uma lista vazia
+	// Para evitar repetições de categorias usar a cláusula DISTINCT
+	
+	@Query(
+			"SELECT DISTINCT objeto FROM Product objeto "
 			+ "INNER JOIN objeto.categories cats "
-			+ "WHERE :category IN cats")
+			+ "WHERE (:category IS NULL OR :category IN cats)"
+			)
 	Page<Product> search(Category category, Pageable pageable);
 
 }
