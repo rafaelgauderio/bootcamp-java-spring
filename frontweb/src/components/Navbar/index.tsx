2cebeb5dfdef49f2f6eb6c0f9674ac30aee302ab
Rafael de Luca <deluca1712@gmail.com>
2022-12-22 15:34:25 -0300

    menu burger on navigation bar

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -1,16 +1,33 @@
 import './styles.css';
+import '@popperjs/core';
+import 'bootstrap/js/src/collapse';
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-md bg-primary main-nav">
+    <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
       <div className="container-fluid">
         <a href="link" className="nav-logo-text">
           <h4>Catálogo de Produtos</h4>
         </a>
-        <div className="collapse navbar-collapse">
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#catalogo-navbar"
+          aria-controls="catalogo-navbar"
+          aria-expanded="false"
+          arial-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="catalogo-navbar">
           <ul className="navbar-nav offset-md-2 main-menu">
             <li>
-              <a href="link" className="active">HOME</a>
+              <a href="link" className="active">
+                HOME
+              </a>
             </li>
             <li>
               <a href="link">CATÁLOGO</a>
