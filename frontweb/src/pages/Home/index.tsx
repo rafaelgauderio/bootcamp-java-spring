d8b73d661349cd9c3521961bc962a48ff068e1b3
Rafael de Luca <deluca1712@gmail.com>
2022-12-26 16:03:54 -0300

    responsiveness - breakpoint 1200px, 1400px

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -10,8 +10,10 @@ const Home = () => {
       <div className="home-container">
         <div className="home-card">
           <div className="home-content-container">
+            <div>
             <h1>Conheça nosso catálogo de produtos</h1>
             <p>Encontre aqui os melhores produtos eletrônicos do mercado</p>
+            </div>
             <ButtonIcon></ButtonIcon>
           </div>
           <div className="home-image-container">
