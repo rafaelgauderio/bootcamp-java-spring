cede7a676fbc844de62e5260d147adc0f9f07cc9
Rafael de Luca <deluca1712@gmail.com>
2022-12-29 10:34:26 -0300

    Routes to Products (Catalog)

--- frontweb/src/Routes.tsx
+++ frontweb/src/Routes.tsx
@@ -1,19 +1,20 @@
-import { BrowserRouter, Switch, Route  } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import NavBar from 'components/Navbar';
 import Home from 'pages/Home';
+import Catalog from 'pages/Home/Catalog';
 
-
-const Rotas = () =>  (
-
-    <BrowserRouter>
-      <NavBar></NavBar>
-      <Switch>
-        <Route path="/" exact>
-          <Home></Home>
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  
+const Rotas = () => (
+  <BrowserRouter>
+    <NavBar></NavBar>
+    <Switch>
+      <Route path="/" exact>
+        <Home></Home>
+      </Route>
+      <Route path="/products">
+        <Catalog></Catalog>
+      </Route>
+    </Switch>
+  </BrowserRouter>
 );
 
 export default Rotas;
--- frontweb/src/pages/Home/Catalog/index.tsx
+++ frontweb/src/pages/Home/Catalog/index.tsx
@@ -1,10 +1,7 @@
-import NavBar from 'components/Navbar';
 import ProductCard from 'components/ProductCard';
 
 const Catalog = () => {
-  return (
-    <>
-      <NavBar></NavBar>
+  return (         
       <div className="container my-4">
         <div className="row">
           <div className="col-sm-6 col-lg-4 col-xl-3">
@@ -33,7 +30,7 @@ const Catalog = () => {
           </div>
         </div>
       </div>
-    </>
+    
   );
 };
 
