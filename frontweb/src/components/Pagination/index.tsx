6e0db9c2cfa040b993dba37499f8c33e33458211
Rafael de Luca <deluca1712@gmail.com>
2023-01-04 11:54:52 -0300

    Pagination styles with css

--- frontweb/src/components/Pagination/index.tsx
+++ frontweb/src/components/Pagination/index.tsx
@@ -1,16 +1,18 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 
+import './styles.css';
+
 const Pagination = () => {
   return (
     <div className="pagination-container">
-      <ArrowIcon></ArrowIcon>
-        <div className="pagination-item">1</div>
+      <ArrowIcon className="arrow-inactive arrow-previus"></ArrowIcon>
+        <div className="pagination-item actived">1</div>
         <div className="pagination-item">2</div>
         <div className="pagination-item">3</div>
         <div className="pagination-item">4</div>
         <div className="pagination-item">...</div>
         <div className="pagination-item">9</div> 
-       <ArrowIcon></ArrowIcon>           
+       <ArrowIcon className="arrow-active arrow-next"></ArrowIcon>           
     </div>
   );
 };
