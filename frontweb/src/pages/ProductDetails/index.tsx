cd10390c5fa9ab9e63774fd48ddf06cf36ab6b4d
Rafael de Luca <deluca1712@gmail.com>
2023-01-04 10:51:07 -0300

    Create link to goBack

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,37 +1,46 @@
-import { ReactComponent as ArrowIcon} from 'assets/images/arrow.svg';
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 import ProductPrice from 'components/ProductPrice';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
 const ProductDetails = () => {
-    
-    return (
-        <div className="product-details-container">
-            <div className="base-card product-details-card">
-                <div className="goback-container">
-                    <ArrowIcon></ArrowIcon>
-                    <h3>VOLTAR</h3>
-                </div>
-                <div className="row">
-                    <div className="col-xl-6">
-                        <div className="img-container">
-                            <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/25-big.jpg" alt="nome do produto"/>
-                        </div>
-                        <div className="name-price-container">
-                            <h1>Nome do Produto</h1>
-                            <ProductPrice price={1234.56}></ProductPrice>
-                        </div>
-                    </div>
-                    <div className="col-xl-6">
-                        <div className="description-container">
-                            <h2>Descrição detalhada do Produto</h2>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid consequuntur magnam provident veritatis? Earum rerum enim sunt recusandae fugiat aut. </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="product-details-container">
+      <div className="base-card product-details-card">
+        <div className="goback-container">
+          <ArrowIcon></ArrowIcon>
+          <Link to="/products">
+            <h3>VOLTAR</h3>
+          </Link>
         </div>
-    );
+        <div className="row">
+          <div className="col-xl-6">
+            <div className="img-container">
+              <img
+                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/25-big.jpg"
+                alt="nome do produto"
+              />
+            </div>
+            <div className="name-price-container">
+              <h1>Nome do Produto</h1>
+              <ProductPrice price={1234.56}></ProductPrice>
+            </div>
+          </div>
+          <div className="col-xl-6">
+            <div className="description-container">
+              <h2>Descrição detalhada do Produto</h2>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Aliquid consequuntur magnam provident veritatis? Earum rerum
+                enim sunt recusandae fugiat aut.{' '}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetails;
