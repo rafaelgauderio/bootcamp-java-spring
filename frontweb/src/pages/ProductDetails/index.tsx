d581a300cd12b0edad690ec162271fe810bd4603
Rafael de Luca <deluca1712@gmail.com>
2023-01-05 11:56:44 -0300

    Request from backend with axios, get productsDetails

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,10 +1,23 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import axios from 'axios';
 import ProductPrice from 'components/ProductPrice';
 import { Link } from 'react-router-dom';
+import { Product } from 'types/product';
+import { BASE_URL } from 'util/requests';
 
 import './styles.css';
 
 const ProductDetails = () => {
+
+  // forma errada de fazer
+  let product : Product;
+
+  // forma incorreta
+  axios.get(BASE_URL + "/products/25")
+    .then( response => {
+        console.log(response.data)
+    });
+
   return (
     <div className="product-details-container">
       <div className="base-card product-details-card">
