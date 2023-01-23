659b8ea18e51f68590c2615d89e299ec4369fd8a
Rafael de Luca <deluca1712@gmail.com>
2023-01-23 12:11:20 -0300

    Children component React.ReactNode

---
+++ frontweb/src/components/TestChildren/index.tsx
@@ -0,0 +1,20 @@
+// Componente pode receber um bloco jsx como argumento
+// analogia a componentes tenha filhos.
+
+type Props = {
+  weight: number;
+  children: React.ReactNode;
+};
+
+const TestChildren = ({ weight, children }: Props) => {
+  return (
+    <>
+    {children}
+      <h3>Testando componente children</h3>
+      <h4>Peso = {weight} </h4>
+      {children}
+    </>
+  );
+};
+
+export default TestChildren;
--- frontweb/src/pages/Tests/index.tsx
+++ frontweb/src/pages/Tests/index.tsx
@@ -1,5 +1,6 @@
 import { ReactComponent as ImagemPrincipal } from 'assets/images/main-image.svg';
 import ButtonIcon from 'components/ButtonIcon';
+import TestChildren from 'components/TestChildren';
 import { Link } from 'react-router-dom';
 import { getTokenData, isUserAuthenticated } from 'util/requests';
 import './styles.css';
@@ -8,6 +9,18 @@ const Tests = () => {
   return (
     <>
       <div className="home-container">
+        <h1>Página para testar novos componentes</h1>
+        <TestChildren weight={74.9}>
+          <h5 text-left>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
+            officia!
+          </h5>
+          <h5>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae nam
+            officia maxime illum optio quia?
+          </h5>
+        </TestChildren>
+        <br />
         <h3>{getTokenData()?.user_name}</h3>
         <h3>
           {isUserAuthenticated()
