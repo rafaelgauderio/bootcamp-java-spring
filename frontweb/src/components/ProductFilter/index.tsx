32ceb6e081a54007be10f58ad4f80e9dc5a24c14
Rafael de Luca <deluca1712@gmail.com>
2023-02-03 15:10:21 -0300

    ProductFilter html

--- frontweb/src/components/ProductFilter/index.tsx
+++ frontweb/src/components/ProductFilter/index.tsx
@@ -2,7 +2,21 @@ import './styles.css';
 
 const ProductFilter = () => {
   return (
-    <div className="base-card search-bar-product-filter">Barra de Busca</div>
+    <div className="base-card search-bar-product-filter">
+      <form action="" className="product-filter-form">
+        <div className="product-filter-name-container">
+          <input type="text" className="form-control"></input>
+        </div>
+        <div className="product-filter-bottom-container">
+          <div className="product-filter-category-container">
+            <select name="" id="">
+              <option value="">Categoria 01</option>
+            </select>
+          </div>
+          <button className="btn btn-outline-secondary">Limpar</button>
+        </div>
+      </form>
+    </div>
   );
 };
 
