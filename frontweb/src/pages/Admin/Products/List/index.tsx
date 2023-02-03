39d50e4bd4c57f646d013dd126062358724051a0
Rafael de Luca <deluca1712@gmail.com>
2023-02-03 12:18:19 -0300

    Create a data control component

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -8,19 +8,35 @@ import { AxiosRequestConfig } from 'axios';
 import './styles.css';
 import Pagination from 'components/Pagination';
 
+type ControlComponentsData = {
+  activePage: number;
+};
+
 const List = () => {
   const [page, setPage] = useState<SpringPage<Product>>();
 
   // essência do padrão de projetos observer
   // fazer um componente que permite que permite outros componentes escrevam no evento dele
-
   //sempre que deleter um produto vai buscar a lista atualiza do backend
-  const getProducts = (pageNumber : number) => {
+
+  // além do estado da pagina, necessário um  outro estado para salvar as info
+  // dos componentes de controle da listagem (paginação e barra de filtragem)
+  const [controlComponentsData, setControlComponentsData] =
+    useState<ControlComponentsData>({
+      activePage: 0,
+    });
+
+  const handlePageChange = (pageNumber: number) => {
+    setControlComponentsData({ activePage: pageNumber }); // atualizar o estado que componente retornar
+  };
+
+  // use Effect roda sempre que o componente é montando e quando as dependências do componente forem alteradas
+  useEffect(() => {
     const params: AxiosRequestConfig = {
       method: 'GET',
       url: '/products',
       params: {
-        page: pageNumber,
+        page: controlComponentsData.activePage,
         size: 5,
       },
     };
@@ -28,11 +44,7 @@ const List = () => {
     requestBackend(params).then((response) => {
       setPage(response.data);
     });
-  };
-
-  useEffect(() => {
-    getProducts(0);
-  }, []);
+  }, [controlComponentsData]);
 
   return (
     <div className="product-crud-container">
@@ -49,22 +61,18 @@ const List = () => {
       <div className="row">
         {page?.content.map((produto) => (
           <div key={produto.id} className="col-sm-6 col-md-12">
-            <ProductCrudCard
-              product={produto}
-              onDelete={() => {
-                getProducts(page.number); //número da página como argumento
-              }}
-            />
+            <ProductCrudCard product={produto} onDelete={() => {}} />
           </div>
         ))}
       </div>
 
-      <Pagination // fazer um if ternário para tratar o caso do page ser undefined
+      <Pagination
+        // Componente Pagination retorna apenas um número informando qual é página ativa
+        // fazer um if ternário para tratar o caso do page ser undefined
         pageCount={page ? page.totalPages : 0}
         range={3}
-        onChange={getProducts} 
+        onChange={handlePageChange}
         // apenas um ponteiro (referência da função) não está chamando a função getProducts()
-        
       />
     </div>
   );
