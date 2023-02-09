585bc02a48e349fd2708625301d963638f3a1544
Rafael de Luca <deluca1712@gmail.com>
2023-02-09 14:37:39 -0300

    Testing Catalog, async-wait and waitFor

--- frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
+++ frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -1,10 +1,11 @@
-import { screen, render } from "@testing-library/react";
+import { screen, render, waitFor } from "@testing-library/react";
 import { Router } from "react-router-dom";
 import Catalog from "..";
 import history from "util/history";
+import { afterWrite } from "@popperjs/core";
 
 
-it("Catalog should render Catalog with products", () => {
+it("Catalog should render Catalog with no products", () => {
 
     render (
         <Router history={history}>
@@ -14,4 +15,26 @@ it("Catalog should render Catalog with products", () => {
 
     // retornar apenas o texto dentro do h1
     expect(screen.getByText("Catálogo de Produtos")).toBeInTheDocument();
+
+})
+
+it("Catalog should render Catalog with products", async () => {
+
+    render (
+        <Router history={history}>
+            <Catalog />
+        </Router>
+    );
+
+    // retornar apenas o texto dentro do h1
+    expect(screen.getByText("Catálogo de Produtos")).toBeInTheDocument();
+    // a exibição dos produtos depende de uma requisição ao backend. A requisição é assincrona.
+    // Senão houverem espeficicações assincronas, os testes vão pegar apenas dados da renderização imediata
+    // agora vai esperar o resultado da função assincrona para continuar a executar o teste
+    await waitFor( () => {
+        expect(screen.getByText("The Lord of the Rings"));
+        expect(screen.getByText("Macbook Pro"));
+    });
+    
+
 })
