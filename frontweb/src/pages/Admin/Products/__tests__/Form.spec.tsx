3b036573eae4207c61ef8a36d9a065689ab5c6cc
Rafael de Luca <deluca1712@gmail.com>
2023-02-10 11:31:14 -0300

    Mocking useParams and react-router-dom to test create Product form

--- frontweb/src/pages/Admin/Products/__tests__/Form.spec.tsx
+++ frontweb/src/pages/Admin/Products/__tests__/Form.spec.tsx
@@ -1,20 +1,35 @@
 import { render, screen } from "@testing-library/react";
-import { Router } from "react-router-dom";
+import { Router, useParams } from "react-router-dom";
 import Form from "../Form";
 import history from "util/history";
 
-it("should render Form component", () => {
+// mockando o react-router-dom
+jest.mock("react-router-dom", () => ({
+    // spread operator para aproveitar o objeto e só sobrescrever uma parte do objeto
+    ...jest.requireActual("react-router-dom"),
+    useParams: jest.fn()
+}));
 
-
-
-    render(
-        <Router history={history}>
-            <Form />
-                    </Router>
-
-
-    )
-
-    screen.debug();
+// para diferenciar create e edit product
+beforeEach(() => {
+    (useParams as jest.Mock).mockReturnValue({
+        productId: "create",
+    });
 });
+
+describe("Form create Product tests", () => {
+
+
+    it("should render Form component", () => {
+
+        render(
+            <Router history={history}>
+                <Form />
+            </Router>
+
+        );
+    });
+
+});
+
 
